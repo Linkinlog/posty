5d7886138cc54d742662f9d588e212e610143d27
Linkinlog <dahlton@linkd.dev>
2021-10-21 04:33:55 -0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('register', [RegisterController::class, 'index'])->name('register');
+
+
+Route::get('/posts', function () {
+    return view('posts.index');
+})->name('posts');
